157025b21721a3b9c4d47b94b3f2ae7dd8394e6f
AymenSekhri <aymen.sekhri@live.fr>
2020-04-19 22:49:25 +0100

    GPIO: Added toggleGPIOBit function
    a simple function is added to toggle a bit in an GPIO directly.

--- STM32F103-HAL/tinyHAL/GPIO.c
+++ STM32F103-HAL/tinyHAL/GPIO.c
@@ -7,6 +7,34 @@ void setGPIOBit(int port,int pin,bool value){
 		setGPIOBitLow (port,pin);
 }
 
+void toggleGPIOBit(int port,int pin){
+	switch(port){
+		case Peripheral_GPIOA:
+			GPIOA->ODR ^=  (1 << pin);
+			break;
+		case Peripheral_GPIOB:
+			GPIOB->ODR ^=  (1 << pin);
+			break;
+		case Peripheral_GPIOC:
+			GPIOC->ODR ^=  (1 << pin);
+			break;
+		case Peripheral_GPIOD:
+			GPIOD->ODR ^=  (1 << pin);
+			break;
+		case Peripheral_GPIOE:
+			GPIOE->ODR ^=  (1 << pin);
+			break;
+		case Peripheral_GPIOF:
+			GPIOF->ODR ^=  (1 << pin);
+			break;
+		case Peripheral_GPIOG:
+			GPIOG->ODR ^=  (1 << pin);
+			break;
+		default:
+			break;
+	}
+}
+
 void setGPIOBitLow(int port,int pin){
 	switch(port){
 		case Peripheral_GPIOA:
@@ -79,3 +107,5 @@ bool readGPIOBit(int port,int pin){
 			break;
 	}
 }
+
+
